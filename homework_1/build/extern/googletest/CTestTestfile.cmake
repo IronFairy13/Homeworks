e18b7d27952fb5d7e3badfe0b6427236a13e934a
Mikhail <savchenko.mn.dev@gmail.com>
2025-05-12 17:00:02 -0300

    change repository

--- homework_1/build/extern/googletest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/savch/Study/Professional_Cpp/otus_cpp/Homeworks/homework_1/extern/googletest
-# Build directory: C:/Users/savch/Study/Professional_Cpp/otus_cpp/Homeworks/homework_1/build/extern/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
